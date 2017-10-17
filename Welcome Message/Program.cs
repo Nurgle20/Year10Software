b98257ee0757e7ea9cdf704325d5eebb265aeef5
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-10-18 09:58:27 +1100

    numberfun

---
+++ Welcome Message/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi Sebastian, welcom to my program1");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ numberfun/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberfun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number X:");
+            float num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number Y:");
+            float num2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(num1 + " plus " + num2 +" = " + (num1 + num2));
+            Console.WriteLine(num1 + " times " + num2 +" = " + num1 * num2);
+            Console.WriteLine(num1 + " minus " + num2 +" = " + (num1 - num2));
+            Console.WriteLine(num1 + " divided " + num2  +" = " + num1 / num2);
+
+            Console.ReadKey();
+        }
+    }
+}
