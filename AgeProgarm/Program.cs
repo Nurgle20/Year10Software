18191fff3ae7e8e0bd654ee09193b7319bd5e0ab
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-10-24 14:55:53 +1100

    lol

---
+++ AgeProgarm/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeProgarm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Age");
+            int age  = int.Parse(Console.ReadLine());
+
+            if (age <= "13")
+            {
+                Console.WriteLine("You're a Child");
+            }
+            else if (age <= "20")
+            {
+                Console.WriteLine("You're a Teen")
+            }
+        }
+    }
+}
---
+++ Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to number....");
+            int number = int.Parse(Console.ReadLine());
+
+            for(int num = 1; num <= number; num++)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("Start");
+            Console.ReadLine();
+
+            int number1 = 1;
+            while(number1 < 11)
+            {
+                Console.WriteLine(number1);
+                number1++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
