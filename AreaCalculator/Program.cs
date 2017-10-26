76542ac0f22f67753ea399f671401e19929d1eaf
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-10-26 12:32:17 +1100

    Area

---
+++ AreaCalculator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice = "";
+            while (choice != "4")
+            {
+                // Menu 
+                Console.WriteLine("choose an Option:");
+                Console.WriteLine("1. Area of Rectangle");
+                Console.WriteLine("2. Area of Triangle");
+                Console.WriteLine("3. Area of Circle");
+                Console.WriteLine("4. Exit");
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    // Area of Rectangle
+                    Console.WriteLine();
+                    Console.Write("width: ");
+                    float width = float.Parse(Console.ReadLine());
+                    Console.Write("Length: ");
+                    float length = float.Parse(Console.ReadLine());
+                    Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Area: " + (width * length));
+                }
+                else if (choice == "2")
+                {
+                    // Area of Triangle
+                    Console.WriteLine();
+                    Console.Write("Base: ");
+                    float width = float.Parse(Console.ReadLine());
+                    Console.Write("Height: ");
+                    float height = float.Parse(Console.ReadLine());
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Area: " + (0.5 * width * height));
+                }
+                else if (choice == "3")
+                {
+                    // Area of Circle
+                    Console.WriteLine();
+                    Console.Write("Radius: ");
+                    float radius = float.Parse(Console.ReadLine());
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Area: " + (Math.PI * radius * radius));
+                }
+                else if (choice == "4")
+                {
+                    break;
+                }
+
+                // wait
+                Console.WriteLine();
+                Console.WriteLine("Press any key to contine...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
