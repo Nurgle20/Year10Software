59a12fc2dc5a36538f5f0ec2b18bc4db773f45d3
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-11-08 10:42:13 +1100

    lol

---
+++ Magic8-Ball/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magic8_Ball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            playGame();
+        }
+
+        private static void playGame()
+        {
+           string[] answers = {"Maybe", "Yes", "No"}
+        }
+    }
+}
