4c63d74faab43ec93838b4b371205d2952893fb2
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-11-09 12:39:05 +1100

    lol

--- Magic8-Ball/Program.cs
+++ Magic8-Ball/Program.cs
@@ -15,7 +15,23 @@ namespace Magic8_Ball
 
         private static void playGame()
         {
-           string[] answers = {"Maybe", "Yes", "No"}
+            string[] answers = {"Maybe", "Yes", "No", "It is certain",
+                                "Unsure, ask again" , "All signs point to yes",
+                               "Most likely", "Reply hazy, try again", "Definitely not" };
+
+
+            Console.WriteLine("Ask no a question that can be answered 'yes' or 'no' . Type 'exit' to leave");
+
+            if(Console.ReadLine() != "exit")
+            {
+                Random rnd = new Random();
+
+                Console.WriteLine(answers[rnd.Next(9)]);
+
+                Console.WriteLine("");
+
+                playGame();
+            }
         }
     }
 }
---
+++ babyBlackjack/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace babyBlackjack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float money = 10;
+            playGame(ref money);
+        }
+        private static void playGame(ref float money)
+        {
+            Console.Clear();
+            float betAmount = MakeBet(ref money);
+            Random rnd = new Random();
+            int playerTotal = DealCards(rnd, "You");
+            int dealerTotal = DealCards(rnd, "The dealer");
+            bool playerWin = determineWinner(playerTotal, dealerTotal);
+            PayOut(playerWin, betAmount, ref money);
+            Menu(ref money);
+        }
+
+
+
+            private static void playGame(ref float money)..
+
+            private static float MakeBet(ref float money)
+        {
+            float betAmount = 0;
+            bool validBet = false;
+            Console.WriteLine("Enter your bet. you have $" + money);
+
+            while (validBet != true)
+            {
+                validBet = float.TryParse(Console.ReadLine(), out betAmount);
+
+                if (betAmount > money)
+                {
+                    Console.WriteLine("Sorry, you do not have enough. Try again.");
+                    validBet = false;
+                }
+                else if (betAmount < 0)
+                {
+                    Console.WriteLine("Sorry, you cannot bet a negative amount. Try again");
+                    validBet = false;
+                }
+             }
+            money ,money - 
+        }
+        private static int DealCards(Random rnd, string player)...
+
+        private static bool determineWinnner(int player, int dealer)...
+
+        private static void PayOut(bool playerWins, float betAmount, ref float money)...
+
+        private static void Menu(ref float money)
+        {
+            Console.Write("Would you like anthoer game? y/n: ");
+            if(Console.ReadLine() == "y")
+            {
+                playGame(ref money)
+            }
+        }
+
+    }
+}
