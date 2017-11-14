b1fdcf833c7cc10e8830105799d08c371ca4f395
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-11-15 10:28:02 +1100

    lol

---
+++ Classes/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Sebastian" , "Vogel", 17);
+            p.Sit();
+            p.Stand();
+        }
+    }
+
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private bool sitting;
+        private int steps;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            sitting = true;
+            steps = 0;
+        }
+
+        public void Sit()
+        {
+            if (sitting)
+            {
+                Console.WriteLine($"{firstName} {lastName} is already sitting");
+            }
+            else
+            {
+                sitting = true;
+            }
+        }
+
+        public void Stand()
+        {
+            if (sitting)
+            {
+                sitting = false;
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} is already standing");
+            }
+        }   
+       
+        
+    }
+
+}
