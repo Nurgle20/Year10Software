c03931b4e318a80871cbd812848b25ff20ec6742
Sebastian Vogel <Sebastian.Vogel@education.nsw.gov.au>
2017-11-27 09:43:05 +1100

    end

--- EndProject/Program.cs
+++ EndProject/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RockPaperScissors
+namespace EndProject
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name + ",welcome to my program!");
+            Console.ReadLine();
+
+            Person p = new Person("Sebastian", "Vogel", 17);
+            p.Sit();
+            p.Walk(100);
+            p.Stand();
+            p.Birthday();
+            p.Walk(100);
+            p.ChangeNamePrompt();
+            p.PrintInfo();
+
+            Console.ReadLine();
+
+            string pick = "";
+            while (pick != "3") ;
+            Console.WriteLine("Press 1 for Rock Paper Scissors Lizard Spock:");
+            Console.WriteLine("Press 2 for BabyBlackJack");
+            Console.WriteLine("Press 3 to Exit");
+
+            if (pick == "1")
+            {
+
+            }
+
             int userScore = 0;
             int computerScore = 0;
             bool repeat = true;
@@ -35,7 +62,7 @@ namespace RockPaperScissors
         private static string UserTurn()
         {
             string choice = "";
-            while (choice != "1" && choice != "2" && choice !="3" && choice !="4" && choice !="5")
+            while (choice != "1" && choice != "2" && choice != "3" && choice != "4" && choice != "5")
             {
                 Console.WriteLine("1: Rock");
                 Console.WriteLine("2: Paper");
@@ -207,8 +234,8 @@ namespace RockPaperScissors
                 {
                     return "Draw";
                 }
-            } 
-           
+            }
+
         }
         private static void GiveFeedback(string reault, string userChoice, string computerChoice)
         {
@@ -217,7 +244,7 @@ namespace RockPaperScissors
         private static bool PlayAgain()
         {
             string choice = "";
-            while (choice != "1" && choice !="2")
+            while (choice != "1" && choice != "2")
             {
                 Console.WriteLine("Play again? 1= Yes, 2 = No");
                 choice = Console.ReadLine();
@@ -243,4 +270,102 @@ namespace RockPaperScissors
             }
         }
     }
+
+
+
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private bool sitting;
+        private int steps;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            sitting = true;
+            steps = 0;
+        }
+
+        public void ChangeName(string firstName, string lastName, string age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            
+        }
+
+        public void ChangeNamePrompt()
+        {
+            Console.WriteLine($"Name is currently {this.firstName} {this.lastName} and is {this.age} years old, enter new name.");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Age: ");
+            string age = Console.ReadLine();
+            this.firstName = firstName;
+            this.lastName = lastName;
+           
+        }
+
+        public void Walk(int steps)
+        {
+            if (sitting)
+            {
+                Console.WriteLine("You can't walk while you are sitting");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} just walked {steps} steps");
+                this.steps += steps;
+            }
+        }
+
+        public void Birthday()
+        {
+            age++;
+        }
+
+        public void PrintInfo()
+        {
+            if (sitting)
+            {
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, is currently sitting" +
+                                  $" and has taken {steps} steps.");
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} is {age} years old, is currently standing" +
+                                  $" and has taken {steps} steps.");
+            }
+        }
+
+        public void Sit()
+        {
+            if (sitting)
+            {
+                Console.WriteLine($"{firstName} {lastName} is already sitting");
+            }
+            else
+            {
+                sitting = true;
+            }
+        }
+
+        public void Stand()
+        {
+            if (sitting)
+            {
+                sitting = false;
+            }
+            else
+            {
+                Console.WriteLine($"{firstName} {lastName} is already standing");
+            }
+        }
+    }
+
 }
